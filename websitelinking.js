a6eb309144dbc09020ae6f0f7750ec41b1b22730
achallion <bhavyarocks19@gmail.com>
2020-11-20 22:36:52 +0530

    complete

---
+++ websitelinking.js
@@ -0,0 +1,10 @@
+$(
+    function () {
+        $("#tictactoebutton").click(function () {
+            window.open("./tic\ tac\ toe/tictactoe.html", "_self");
+        });
+
+        $("#rgbgamebutton").click(function () {
+            window.open("./rgb\ colour\ guess\ game/rgb.html", "_self");
+        });
+    });
